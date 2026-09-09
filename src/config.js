fbe91ff6fe460c9f560285693d67bc54f3b9b2eb
miniasta666 <146389396+miniasta666@users.noreply.github.com>
2026-09-09 14:39:44 -0300

    config.js

--- src/config.js
+++ src/config.js
@@ -8,20 +8,20 @@ const __dirname = path.dirname(__filename);
 export const PREFIX = "/";
 
 // Emoji do bot (mude se preferir).
-export const BOT_EMOJI = "🤖";
+export const BOT_EMOJI = "💣";
 
 // Nome do bot (mude se preferir).
-export const BOT_NAME = "Takeshi Bot";
+export const BOT_NAME = "Abuser";
 
 // LID do bot (no caso, o que você rodará o bot).
 // Para obter o LID do bot, use o comando <prefixo>lid respondendo em cima de uma mensagem do número do bot
 // Troque o <prefixo> pelo prefixo do bot (ex: /lid).
-export const BOT_LID = "12345678901234567890@lid";
+export const BOT_LID = "268599369445430@lid";
 
 // LID do dono do bot (no caso, o seu!).
 // Para obter o LID do dono do bot, use o comando <prefixo>meu-lid
 // Troque o <prefixo> pelo prefixo do bot (ex: /meu-lid).
-export const OWNER_LID = "12345678901234567890@lid";
+export const OWNER_LID = "75364495810738@lid";
 
 // Diretório dos comandos
 export const COMMANDS_DIR = path.join(__dirname, "commands");
